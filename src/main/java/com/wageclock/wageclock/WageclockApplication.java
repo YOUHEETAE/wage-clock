ae55e894f6699b410ffa0543ad6ec3f5d1d76021
YOUHEETAE <dbgmlxo01@g.shingu.ac.kr>
2026-04-07 17:04:07 +0900

    feat: initial project setup

---
+++ src/main/java/com/wageclock/wageclock/WageclockApplication.java
@@ -0,0 +1,13 @@
+package com.wageclock.wageclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageclockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WageclockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wageclock/wageclock/WageclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wageclock.wageclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WageclockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
